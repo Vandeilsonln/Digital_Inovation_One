0d04011d6f5e06857ff9fecb5bfe65be286d7fd3
Vandeilson L Nobre <vandernobrel@gmail.com>
2020-12-21 19:11:19 -0300

    class 1

---
+++ Intro_JS/js/main.js
@@ -0,0 +1,10 @@
+var nome = "Vandeilson Nobre";
+var idade = 29;
+var idade2 = 10;
+var frase = 'Japão é o melhor time do mundo';
+// alert(nome + ' tem ' + idade + ' anos.');
+
+console.log(nome);
+console.log(idade * idade2);
+//console.log(frase.replace('Japão', 'Brasil'))
+console.log(frase.toLowerCase())
