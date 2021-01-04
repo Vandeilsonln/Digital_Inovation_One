9293c3ce9a37dd5fe3b819a9b085fe386ad0c155
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-03 21:56:11 -0300

    inserir

--- DotNET_primeiros_passos/Revisao/Program.cs
+++ DotNET_primeiros_passos/Revisao/Program.cs
@@ -6,6 +6,8 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
             string opcaoUsuario = ObterOpcaoUsuario();
 
             while (opcaoUsuario.ToUpper() != "X")
@@ -13,6 +15,23 @@ namespace Revisao
                 switch (opcaoUsuario)
                 {
                     case "1":
+                    Console.WriteLine("Informe o nome do aluno:");
+                    
+                    Aluno aluno = new Aluno();
+                    aluno.Nome = Console.ReadLine();
+                    
+                    Console.WriteLine("Informe a nota do aluno:");
+                    if (decimal.TryParse(Console.ReadLine(),out decimal nota))
+                    {
+                        aluno.Nota = nota;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("O valor da nota deve ser decimal");
+                    }
+                    alunos[indiceAluno] = aluno;
+                    indiceAluno++;
+                    
                         break;
 
                     case "2":
@@ -24,11 +43,14 @@ namespace Revisao
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
+                opcaoUsuario = ObterOpcaoUsuario();
+                Console.WriteLine();
             }
         }
 
         private static string ObterOpcaoUsuario()
         {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada:");
             Console.WriteLine("1 - Inserir novo aluno");
             Console.WriteLine("2 - Listar alunos");
