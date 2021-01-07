656f4b8a54c74e5892394e205359c25c4e908336
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-06 22:56:41 -0300

    update

--- DotNET_primeiros_passos/dio_desafios/Program.cs
+++ DotNET_primeiros_passos/dio_desafios/Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
-using System.Globalization;
 
 namespace dio_desafios
 {
     class Desafio {
     static void Main() {
-        int limit = Int32.Parse(Console.ReadLine());
-        NumberFormatInfo nfi = new CultureInfo( "en-US", false ).NumberFormat;
-        for (int i = 0; i < limit; i++) 
+        string[] line = Console.ReadLine().Split(" ");
+        int a = Int32.Parse(line[0]);
+        int n = Int32.Parse(line[1]);
+        double parcialPlacas = 0;
+
+        for(int progresso = 1; progresso <= 9; progresso++)
         {
-            string[] line = Console.ReadLine().Split(" ");
-            double X = double.Parse(line[0]);
-            double Y = double.Parse(line[1]);
-            if (Y == 0) {
-                Console.WriteLine("divisao impossivel");
-            } else {
-                double divisao = X / Y; // Digite aqui o calculo da divisao
-                Console.WriteLine(divisao.ToString("N1", nfi));
-            }
+            parcialPlacas = Math.Ceiling(Convert.ToDouble(a * n * (progresso / 10)));
+            Console.WriteLine(parcialPlacas);
         }
         }
     }
 }
+
+
+// for (int i = 0; i < limit; i++) 
+//         {
+//             string[] line = Console.ReadLine().Split(" ");
+//             double X = double.Parse(line[0]);
+//             double Y = double.Parse(line[1]);
+//             if (Y == 0) {
+//                 Console.WriteLine("divisao impossivel");
+//             } else {
+//                 double divisao = X / Y; // Digite aqui o calculo da divisao
+//                 Console.WriteLine(divisao.ToString("N1", nfi));
+//             }
+//         }
