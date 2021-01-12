1a864fd52947eca093a7c77f15e1ccedf4c4e3f6
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-01-12 15:20:47 -0300

    tratativa de erros

--- WebAPI/curso/curso.api/Controllers/UsuarioController.cs
+++ WebAPI/curso/curso.api/Controllers/UsuarioController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using curso.api.Models;
 using curso.api.Models.Usuarios;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace curso.api.Controllers
 {
@@ -12,6 +14,17 @@ namespace curso.api.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        /// <summary>
+        /// Este serviço permite autenticar um usuário cadastrado e ativo
+        /// teste
+        /// </summary>
+        /// <param name="loginViewModelInput">View Model do login</param>
+        /// <returns>Retorna status ok, dados do usuário e token em caso de sucesso</returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description: "Erro interno", Type = typeof(ErroGenericoViewModel))]
+
+
         [HttpPost]
         [Route("logar")]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
